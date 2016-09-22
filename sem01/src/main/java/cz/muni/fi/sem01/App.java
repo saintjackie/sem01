465cab086bf9a59fabf15df369232a8e737a911d
Jakub Holy <saintjackie@gmail.com>
2016-09-22 11:09:32 +0200

    hello world app

---
+++ sem01/src/main/java/cz/muni/fi/sem01/App.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.sem01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
